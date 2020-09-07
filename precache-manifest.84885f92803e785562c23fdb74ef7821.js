1956ff016656d746578ccf80461a6ff1800da0ed
ravi2615 <67025166+ravi2615@users.noreply.github.com>
2020-09-07 03:09:44 -0700

    Updates

---
+++ precache-manifest.84885f92803e785562c23fdb74ef7821.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c517ccd4830d137a3e02624d9a9fed0",
+    "url": "/ecommerce/index.html"
+  },
+  {
+    "revision": "e2f9c3e803da534ee70e",
+    "url": "/ecommerce/static/css/2.b4bf6411.chunk.css"
+  },
+  {
+    "revision": "fdebd050f80ce1f43161",
+    "url": "/ecommerce/static/css/main.38ac5cad.chunk.css"
+  },
+  {
+    "revision": "e2f9c3e803da534ee70e",
+    "url": "/ecommerce/static/js/2.03ed7dc8.chunk.js"
+  },
+  {
+    "revision": "d7cec49cd6c35c1ed79ef6c2313a0b51",
+    "url": "/ecommerce/static/js/2.03ed7dc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdebd050f80ce1f43161",
+    "url": "/ecommerce/static/js/main.c4b66d5d.chunk.js"
+  },
+  {
+    "revision": "831a87ab3375c7c427c2",
+    "url": "/ecommerce/static/js/runtime-main.ee40b3dc.js"
+  },
+  {
+    "revision": "461080d46251e63a1fea084d5ecf1c13",
+    "url": "/ecommerce/static/media/7d471656-5ffa-411d-b3a7-f7ee34868d94 (1).461080d4.jpg"
+  },
+  {
+    "revision": "6bf67e4e09b3e09a9f2255653a446f1b",
+    "url": "/ecommerce/static/media/slide-1.6bf67e4e.png"
+  }
+]);
