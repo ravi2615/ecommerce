0ac4186b073c158796aade2293943ede75b45cc1
ravi2615 <67025166+ravi2615@users.noreply.github.com>
2020-09-07 03:00:49 -0700

    added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDom from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import "./index.css";
+
+import Main from "./Main";
+
+ReactDom.render(
+    <>
+        <BrowserRouter>
+             <Main/>
+        </BrowserRouter>
+    </>,document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
